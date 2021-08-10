7cd9144ef400957988f71e4a91dc78d7acfea24f
barti132 <barteksredzinski@gmail.com>
2021-08-10 18:10:18 +0200

    Initial commit

---
+++ src/test/java/pl/bartoszsredzinski/todorestapi/TodoRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.bartoszsredzinski.todorestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
